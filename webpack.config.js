23b850b52d3ec2000ed82b5fa4dd3d94a8a305ea
Restyled.io <commits@restyled.io>
2020-06-02 17:20:40 +0000

    Restyled by prettier

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,25 @@ const webpack = require('webpack')
 const mode = process.env.NODE_ENV || 'production'
 
 module.exports = {
-  output : {
-    filename : `worker.${mode}.js`,
-    path : path.join(__dirname, 'dist'),
+  output: {
+    filename: `worker.${mode}.js`,
+    path: path.join(__dirname, 'dist'),
   },
   mode,
-  resolve : {
-    extensions : [ '.ts', '.tsx', '.js' ],
-    plugins : [],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [],
   },
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
-        test : /\.tsx?$/,
-        loader : 'ts-loader',
-        options : {
-          transpileOnly : true,
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
         },
       },
-      {enforce : 'pre', test : /\.js$/, loader : 'source-map-loader'},
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
     ],
   },
 }
